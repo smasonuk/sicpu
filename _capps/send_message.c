ad3bfc61e723d5bfea34e86163e68a75fedca32e
Simon Mason <simon.mason@syngenta.com>
2026-02-26 21:06:19 +0000

    refactor: Clean up whitespace and remove unnecessary comment in send_message.c

--- _capps/send_message.c
+++ _capps/send_message.c
@@ -3,10 +3,8 @@
 
 int main() {
     int slot = find_peripheral("MSGSNDR");
-
     byte* target = "Central Command";
-
-    byte* payload = "Ground control to major Tom"; // This will set the first 4 bytes of payload to 'A', 'B', 'C', 'D'
+    byte* payload = "Ground control to major Tom";
     int len = strlen(payload);
 
     send_msg(slot, target, payload, len);
